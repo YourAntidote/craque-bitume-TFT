1dd384f3fa08a89345cae9dea3462a2f5c570ecd
Antoine Haddad <89608252+YourAntidote@users.noreply.github.com>
2023-09-25 17:53:46 -0400

    Update hero

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,31 @@
 
 /* ----------------- SECTION PAGE ACCUEIL ----------------------- */
 
+const swiper = new Swiper(".swiper-screenshots", {
+  direction: "horizontal",
+  loop: true,
+  centeredSlides: true,
+  
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  grabCursor: true,
+  slidesPerView: 1,
+  spaceBetween: 25,
+  watchSlidesVisibility: true,
+  
+  breakpoints: {
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+
+  
+
+});
+
 /* ----------------- SECTION PAGE NOUVELLE ---------------------- */
 
 /* ----------------- SECTION PAGE SERVICE ----------------------- */
@@ -16,5 +41,5 @@
 /* ----------------- SECTION PAGE 404 --------------------------- */
 let btn_return = document.getElementById("return-btn");
 btn_return.addEventListener("click", function () {
-  window.location.href = "1accueil.html";
+  window.location.href = "index.html";
 });
