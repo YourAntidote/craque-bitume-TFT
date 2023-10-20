160f5d64c88654beeab360e2014aa8f419889bc9
Yarata <70409693+YarataDarkness@users.noreply.github.com>
2023-10-20 10:37:50 -0400

    localstorage
    check the code

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
 /* ----------------- SECTION BANNER X -------------------------------- */
+const isbanner = document.querySelector(".banner");
+
+localStorage.setItem("isbanner");
+
 document.querySelector(".banner__close").addEventListener("click", function() {
   this.closest(".banner").style.display = "none";
+  localStorage.clear("isbanner");
 });
+
+
+/*if (localStorage.clear("isbanner") == true)  {
+  delete("isbanner");
+} Je ne suis pas sure de ce code. Je ne suis pas sure que ca peut faire en sorte que le banner X sera enlever toujours dans la meme session utilisateur*/
+
+
 
 /* ----------------- SECTION NAV -------------------------------- */
 
