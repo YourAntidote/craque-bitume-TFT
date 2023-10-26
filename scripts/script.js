d17b4cefec3d7170aa26f4e2b2fbd64cbe172ad7
YarataDarkness <70409693+YarataDarkness@users.noreply.github.com>
2023-10-26 10:18:58 -0400

    json test

--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,13 @@ const isbanner = document.querySelector(".banner");
 
 document.querySelector(".banner__close").addEventListener("click", function() {
   this.closest(".banner").style.display = "none";
- /* localStorage.clear("isbanner");*/
+  localStorage.clear("isbanner");
 });
 
 
-/*if (localStorage.clear("isbanner") == true)  {
+if (localStorage.clear("isbanner") == true)  {
   delete("isbanner");
-} Je ne suis pas sure de ce code. Je ne suis pas sure que ca peut faire en sorte que le banner X sera enlever toujours dans la meme session utilisateur*/
+}
 
 
 
